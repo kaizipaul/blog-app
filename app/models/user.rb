c2a54b2beecb5be79ff1f6ee807626837b47eb0a
kaizipaul <kaizipaul@gmail.com>
2023-02-09 17:39:16 +0300

    Initialize post counter to zero

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,16 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: 'Post', foreign_key: 'author_id'
-  has_many :likes, foreign_key: 'author_id'
-  has_many :comments, foreign_key: 'author_id'
+  has_many :posts
+  has_many :likes
+  has_many :comments
+
+
+  after_initialize :set_post_counter_default
+
+  def set_post_counter_default
+    self.post_counter = 0 if post_counter.nil?
+  end
+
+  private :set_post_counter_default
 
   def three_posts
     posts.order(created_at: :desc).limit(3)
