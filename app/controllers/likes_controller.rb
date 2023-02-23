04a74543b651aaf9867e81b19cad19a28155a13e
kaizipaul <kaizipaul@gmail.com>
2023-02-23 21:23:13 +0300

    Add devise features to controllers

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,8 +1,8 @@
 class LikesController < ApplicationController
   def create
-    @user = current_user
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:post_id])
-    @like = Like.new(author: current_user, post_id: params[:post_id])
+    @like = Like.new(author: @user, post_id: params[:post_id])
     @like.author_id = @user.id
     @like.post_id = params[:post_id]
     redirect_to user_post_path(@user, @post) if @like.save
